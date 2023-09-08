edda2576a71643e10c86fdcec4935ce7330902a7
Gonzalo Velasco <vela8624@gmail.com>
2023-09-08 18:21:24 -0300

    create test user

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,11 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 async function main() {
-  }
-  
-  main()
-    .then(async () => {
-      console.log("Done seeding the database!");
-    })
-    .catch(async (e) => {
-      console.error(e);
-      process.exit(1);
-    });
+  await prisma.user.upsert({
+    where: { email: "test@test.com" },
+    update: {},
+    create: {
+      email: "test@test.com",
+      name: "test",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("Done seeding the database!");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
