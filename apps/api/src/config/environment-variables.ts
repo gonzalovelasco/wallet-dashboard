99430239a4882d85ecbea25da1a70f730e439856
Gonzalo Velasco <vela8624@gmail.com>
2023-09-18 14:57:18 -0300

    add envs nestjs

--- apps/api/src/config/environment-variables.ts
+++ apps/api/src/config/environment-variables.ts
@@ -1,9 +1,11 @@
-import * as Joi from 'joi';
+import * as Joi from "joi";
 
 export interface EnvironmentVariables {
   DATABASE_URL: string;
+  ETHERSCAN_API_KEY: string;
 }
 
 export const validationSchemaForEnv = Joi.object<EnvironmentVariables, true>({
   DATABASE_URL: Joi.string().required(),
+  ETHERSCAN_API_KEY: Joi.string().required(),
 });
